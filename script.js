d84c8c690d3f25b21c4298868882fed690c51bc1
michael <91875751+michaelll22@users.noreply.github.com>
2025-09-16 11:48:50 +0800

    refactor renderCells and GameController for improved player turn display

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ function CreateBoard() {
 }
 
 function renderCells() {
-  const board = CreateBoard().getBoard();
+  const createBoard = CreateBoard();
+  const game = GameController();
   const boardDiv = document.querySelector(".board");
   const turn = document.querySelector(".turn");
 
   const render = () => {
-    turn.textContent = `'s Turn`;
+    const activePlayer = game.getActivePlayer();
+    turn.textContent = `${activePlayer.name}'s Turn`;
+
+    board = createBoard.getBoard();
     board.forEach((cell, index) => {
       const cellButton = document.createElement("button");
       cellButton.classList.add("cell");
@@ -32,6 +36,33 @@ function renderCells() {
   };
 
   render();
+}
+
+function GameController(playerOne = "p1", playerTwo = "p2") {
+  const board = CreateBoard();
+  let players = [
+    {
+      name: playerOne,
+      marker: "O",
+    },
+    {
+      name: playerTwo,
+      marker: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  return {
+    switchPlayer,
+    getActivePlayer,
+  };
 }
 
 renderCells();
